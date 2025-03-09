bf47d97221f9db953970a3946e0fd3f7d446f214
Wallace Nascimento <devwallacenascimento@gmail.com>
2025-03-08 23:22:35 -0300

    utilizando o Webstorm

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <>
-      <h1>Testando novamente mas rodando o VS Code localmente.</h1>
+      <h1>Testando novamente mas rodando o Webstorm localmente.</h1>
     </>
   );
 }
